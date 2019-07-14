991a50d543e224efd9423d291618b66bb5432fab
Henryck <genapogosyan23@gmail.com>
2019-07-15 00:18:59 +0400

    create structure of menu

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('name');
+const startButton = document.querySelector('.start-button');
+
+startButton.addEventListener('click', () => {
+	event.preventDefault();
+	console.log('Start Game');
+})
